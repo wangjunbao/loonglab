320829a78e3e26276548be6cebc28c2a79a36d0d
liugangc <liugangc@fc796aec-02eb-11de-af5c-836599625e1f>
2009-09-18 08:46:24 +0000

    开始编写采集指数代码

--- gaeExample/src/liugangc/appspot/example/GaeExampleServlet.java
+++ gaeExample/src/liugangc/appspot/example/GaeExampleServlet.java
@@ -1,14 +1,20 @@
 package liugangc.appspot.example;
-import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+import java.io.InputStream;
 import java.net.URL;
 
 import javax.jdo.PersistenceManager;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.xml.sax.InputSource;
 
 import com.google.appengine.repackaged.org.apache.commons.logging.Log;
 import com.google.appengine.repackaged.org.apache.commons.logging.LogFactory;
@@ -20,36 +26,54 @@ public class GaeExampleServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
-		String webSite="http://www.csindex.com.cn/sseportal/csiportal/zs/jbxx/daily_index_info.jsp";
+		String urlStr="http://www.csindex.com.cn/sseportal/ps/zhs/hqjt/csi/indexinfo.xls";
+		log.info(urlStr);
+		System.out.println("===="+urlStr);
 		
 		try {
-            URL url = new URL(webSite);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
+			
+            URL url = new URL(urlStr);
+            InputStream is=url.openStream();
+            POIFSFileSystem fs = new POIFSFileSystem(is);
+			HSSFWorkbook wb = new HSSFWorkbook(fs);
+			HSSFSheet sheet = wb.getSheetAt(0);
+			//System.out.println(sheet.getLastRowNum());
+			for (int i = 4; i < 11 ; i++) {
+				HSSFRow row = sheet.getRow(i);
+				for (int j = 0; j <= 13; j++) {
+					HSSFCell cell=row.getCell(j);
+					
+					if(cell.getCellType()==HSSFCell.CELL_TYPE_NUMERIC)
+						System.out.println(cell.getNumericCellValue());
+					else if(cell.getCellType()==HSSFCell.CELL_TYPE_STRING)
+						System.out.println(cell.getRichStringCellValue().getString());
+						
+					//System.out.println(cell.getCellType());
+				}
+				
+				//log.info("=====================");
+			}
+			
+			is.close();
+			
+			
 
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-            reader.close();
-
-        } catch (MalformedURLException e) {
-            log.error(e.getMessage(),e);
-        } catch (IOException e) {
+        } catch (Exception e) {
             log.error(e.getMessage(),e);
         }
 		
-		StIndex entry=new StIndex();
-		entry.setIndexNum(3000);
-		entry.setPb(3.2);
-		entry.setPe(32.3);
-		
-		
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-	    pm.makePersistent(entry);
-		
-		resp.setContentType("text/plain");
-		resp.getWriter().println("inser a entry("+entry.getId()+")");
-		
-		System.out.println("gaeExampleServlet("+entry.getId()+") end!");
+//		StIndex entry=new StIndex();
+//		entry.setIndexNum(3000);
+//		entry.setPb(3.2);
+//		entry.setPe(32.3);
+//		
+//		
+//		PersistenceManager pm = PMF.get().getPersistenceManager();
+//	    pm.makePersistent(entry);
+//		
+//		resp.setContentType("text/plain");
+//		resp.getWriter().println("inser a entry("+entry.getId()+")");
+//		
+//		System.out.println("gaeExampleServlet("+entry.getId()+") end!");
 	}
 }
--- gaeExample/src/liugangc/appspot/example/StIndex.java
+++ gaeExample/src/liugangc/appspot/example/StIndex.java
@@ -1,5 +1,7 @@
 package liugangc.appspot.example;
 
+import java.util.Date;
+
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
@@ -13,39 +15,140 @@ public class StIndex {
 	@Persistent(valueStrategy=IdGeneratorStrategy.IDENTITY)
 	private Long Id;
 	
-	@Persistent
-	private double indexNum;
+	/**
+	 * 发布日期
+	 */
+	private Date pubDate;
+	
+	/**
+	 * 名称
+	 */
+	private String name;
 	
 	@Persistent
-	private double pe;
+	private double closing;
+	
+	private double upDown;
+	
+	private double upDownPercent;
+	
+	private double yearUpDown;
+	
+	private double yearUpDownPercent;
+	
+	private double turnOverUpDown;
+	
+	private double turnOverUpDownPercent;	
+		
+	@Persistent
+	private double staticPE;
+	
+	private double dynaPE;	
 	
 	@Persistent
-	private double pb;
+	private double PB;
 	
+	private double lastYearStaticPE;
+	private double lastYearDynaPE;
+	private double lastYearPB;
 	public Long getId() {
 		return Id;
 	}
 	public void setId(Long id) {
 		Id = id;
 	}
-	public double getIndexNum() {
-		return indexNum;
+	public Date getPubDate() {
+		return pubDate;
 	}
-	public void setIndexNum(double indexNum) {
-		this.indexNum = indexNum;
+	public void setPubDate(Date pubDate) {
+		this.pubDate = pubDate;
 	}
-	public double getPe() {
-		return pe;
+	public String getName() {
+		return name;
 	}
-	public void setPe(double pe) {
-		this.pe = pe;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public double getPb() {
-		return pb;
+	public double getClosing() {
+		return closing;
 	}
-	public void setPb(double pb) {
-		this.pb = pb;
+	public void setClosing(double closing) {
+		this.closing = closing;
 	}
+	public double getUpDown() {
+		return upDown;
+	}
+	public void setUpDown(double upDown) {
+		this.upDown = upDown;
+	}
+	public double getUpDownPercent() {
+		return upDownPercent;
+	}
+	public void setUpDownPercent(double upDownPercent) {
+		this.upDownPercent = upDownPercent;
+	}
+	public double getYearUpDown() {
+		return yearUpDown;
+	}
+	public void setYearUpDown(double yearUpDown) {
+		this.yearUpDown = yearUpDown;
+	}
+	public double getYearUpDownPercent() {
+		return yearUpDownPercent;
+	}
+	public void setYearUpDownPercent(double yearUpDownPercent) {
+		this.yearUpDownPercent = yearUpDownPercent;
+	}
+	public double getTurnOverUpDown() {
+		return turnOverUpDown;
+	}
+	public void setTurnOverUpDown(double turnOverUpDown) {
+		this.turnOverUpDown = turnOverUpDown;
+	}
+	public double getTurnOverUpDownPercent() {
+		return turnOverUpDownPercent;
+	}
+	public void setTurnOverUpDownPercent(double turnOverUpDownPercent) {
+		this.turnOverUpDownPercent = turnOverUpDownPercent;
+	}
+	public double getStaticPE() {
+		return staticPE;
+	}
+	public void setStaticPE(double staticPE) {
+		this.staticPE = staticPE;
+	}
+	public double getDynaPE() {
+		return dynaPE;
+	}
+	public void setDynaPE(double dynaPE) {
+		this.dynaPE = dynaPE;
+	}
+	public double getPB() {
+		return PB;
+	}
+	public void setPB(double pb) {
+		PB = pb;
+	}
+	public double getLastYearStaticPE() {
+		return lastYearStaticPE;
+	}
+	public void setLastYearStaticPE(double lastYearStaticPE) {
+		this.lastYearStaticPE = lastYearStaticPE;
+	}
+	public double getLastYearDynaPE() {
+		return lastYearDynaPE;
+	}
+	public void setLastYearDynaPE(double lastYearDynaPE) {
+		this.lastYearDynaPE = lastYearDynaPE;
+	}
+	public double getLastYearPB() {
+		return lastYearPB;
+	}
+	public void setLastYearPB(double lastYearPB) {
+		this.lastYearPB = lastYearPB;
+	}
+	
+	
 	
 	
 }
