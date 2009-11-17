bc8d118e665430bfc338316f0f828073cab4aad9
liugangc <liugangc@fc796aec-02eb-11de-af5c-836599625e1f>
2009-11-17 09:28:47 +0000

    添加接口

---
+++ lbs/src/com/kejikeji/lbs/web/UserAction.java
@@ -0,0 +1,17 @@
+package com.kejikeji.lbs.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+
+import com.kejikeji.common.web.BaseMultiActionController;
+
+public class UserAction extends BaseMultiActionController {
+	public ModelAndView databasePropertyMain( HttpServletRequest request, HttpServletResponse response ){
+		
+		return null;
+		
+	}
+}
