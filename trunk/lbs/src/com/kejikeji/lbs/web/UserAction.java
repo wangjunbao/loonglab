8c083595f7be99d482fd163752abf2b2f343c4f2
liugangc <liugangc@fc796aec-02eb-11de-af5c-836599625e1f>
2009-11-17 13:59:35 +0000

    添加servlet接口

--- trunk/lbs/src/com/kejikeji/lbs/web/UserAction.java
+++ trunk/lbs/src/com/kejikeji/lbs/web/UserAction.java
@@ -1,15 +1,55 @@
 package com.kejikeji.lbs.web;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import com.kejikeji.common.JsonUtil;
 import com.kejikeji.common.web.BaseMultiActionController;
+import com.kejikeji.lbs.model.User;
+import com.kejikeji.lbs.service.UserService;
 
 public class UserAction extends BaseMultiActionController {
-	public ModelAndView databasePropertyMain( HttpServletRequest request, HttpServletResponse response ){
+	
+	private UserService userService;
+	
+	
+	
+	public UserService getUserService() {
+		return userService;
+	}
+
+
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+
+
+	public ModelAndView login( HttpServletRequest request, HttpServletResponse response ){
+		
+		String userName=request.getParameter("userName");
+		String password=request.getParameter("password");
+		
+		User user=userService.login(userName, password);
+		
+		if(user==null){
+			response.setStatus(400);
+		}
+		
+		//TODO 10021需要定义常量
+		try {
+			response.getWriter().print(JsonUtil.getJsonString("result", "10021"));
+			response.flushBuffer();
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage(),e);
+		}
 		
 		return null;
 		
---
+++ trunk/lbs/test/org/json/JSONObjectTest.java
@@ -0,0 +1,267 @@
+package org.json;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import junit.framework.TestCase;
+
+public class JSONObjectTest extends TestCase {
+	
+	static Log log=LogFactory.getLog(JSONObject.class);
+
+	public void testJSONObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testJSONObjectJSONObjectStringArray() {
+		fail("Not yet implemented");
+	}
+
+	public void testJSONObjectJSONTokener() {
+		fail("Not yet implemented");
+	}
+
+	public void testJSONObjectMap() {
+		fail("Not yet implemented");
+	}
+
+	public void testJSONObjectMapBoolean() {
+		fail("Not yet implemented");
+	}
+
+	public void testJSONObjectObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testJSONObjectObjectBoolean() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsStandardProperty() {
+		fail("Not yet implemented");
+	}
+
+	public void testJSONObjectObjectStringArray() {
+		fail("Not yet implemented");
+	}
+
+	public void testJSONObjectString() {
+		fail("Not yet implemented");
+	}
+
+	public void testAccumulate() {
+		fail("Not yet implemented");
+	}
+
+	public void testAppend() {
+		fail("Not yet implemented");
+	}
+
+	public void testDoubleToString() {
+		fail("Not yet implemented");
+	}
+
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetBoolean() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetDouble() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetJSONArray() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetJSONObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetLong() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetNamesJSONObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetNamesObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetString() {
+		fail("Not yet implemented");
+	}
+
+	public void testHas() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsNull() {
+		fail("Not yet implemented");
+	}
+
+	public void testKeys() {
+		fail("Not yet implemented");
+	}
+
+	public void testLength() {
+		fail("Not yet implemented");
+	}
+
+	public void testNames() {
+		fail("Not yet implemented");
+	}
+
+	public void testNumberToString() {
+		fail("Not yet implemented");
+	}
+
+	public void testOpt() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptBooleanString() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptBooleanStringBoolean() {
+		fail("Not yet implemented");
+	}
+
+	public void testPutStringCollection() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptDoubleString() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptDoubleStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptIntString() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptIntStringInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptJSONArray() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptJSONObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptLongString() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptLongStringLong() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptStringString() {
+		fail("Not yet implemented");
+	}
+
+	public void testOptStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	public void testPutStringBoolean() {
+		fail("Not yet implemented");
+	}
+
+	public void testPutStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	public void testPutStringInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testPutStringLong() {
+		fail("Not yet implemented");
+	}
+
+	public void testPutStringMap() {
+		fail("Not yet implemented");
+	}
+
+	public void testPutStringObject() throws Exception{
+		JSONObject json=new JSONObject();
+		json.put("resultCode", "10021");
+		log.info(json.toString());
+		//fail("Not yet implemented");
+	}
+
+	public void testPutOnce() {
+		fail("Not yet implemented");
+	}
+
+	public void testPutOpt() {
+		fail("Not yet implemented");
+	}
+
+	public void testQuote() {
+		fail("Not yet implemented");
+	}
+
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	public void testSortedKeys() {
+		fail("Not yet implemented");
+	}
+
+	public void testStringToValue() {
+		fail("Not yet implemented");
+	}
+
+	public void testTestValidity() {
+		fail("Not yet implemented");
+	}
+
+	public void testToJSONArray() {
+		fail("Not yet implemented");
+	}
+
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	public void testToStringInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testToStringIntInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testValueToStringObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testValueToStringObjectIntInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testWrite() {
+		fail("Not yet implemented");
+	}
+
+}
