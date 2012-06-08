751cb2d7990b936b79ab8986a89d2d6938605791
liugangc@gmail.com <liugangc@gmail.com@fc796aec-02eb-11de-af5c-836599625e1f>
2012-06-08 09:17:58 +0000

    查询代码

--- hdc/src/org/loonglab/hdc/imp/query/GroupService.java
+++ hdc/src/org/loonglab/hdc/imp/query/GroupService.java
@@ -1,5 +1,14 @@
 package org.loonglab.hdc.imp.query;
 
+import java.util.List;
+import java.util.Map;
+
 public class GroupService {
-	
+	public static List<Map<String, String>> groupDoc(PageBean page, String tenantId, 
+			String type, Map<String, String> map_query, 
+			String s_select, String group_by,String order_by,String notnull){
+		
+		
+		return null;
+	}
 }
