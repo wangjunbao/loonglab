de1ab1d38e385d2a2823625aef518f504718dedd
liugangc@gmail.com <liugangc@gmail.com@fc796aec-02eb-11de-af5c-836599625e1f>
2012-10-23 05:56:45 +0000

    filter

--- hdc/src/org/loonglab/hdc/imp/query/GroupService.java
+++ hdc/src/org/loonglab/hdc/imp/query/GroupService.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.BinaryComparator;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.RowFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.loonglab.hdc.common.HTablePoolFactory;
 
@@ -19,12 +22,22 @@ public class GroupService {
 		ResultScanner scanner=null;
 		HTableInterface table=null;
 		try {
-			Scan scan=new Scan();
+			Scan scan=new Scan(Bytes.toBytes("245_baidu_keywordROI_2012-02-09_"),Bytes.toBytes("245_baidu_keywordROI_2012-02-09_999999999"));
+			scan.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("tenantId"));
+//			
+//			Filter filter1 = new RowFilter(CompareFilter.CompareOp.LESS, 
+//				      new BinaryComparator(Bytes.toBytes("245_baidu_keywordROI_2012-02-09_999999999")));
+//			Filter filter2 = new RowFilter(CompareFilter.CompareOp.GREATER, 
+//				      new BinaryComparator(Bytes.toBytes("245_baidu_keywordROI_2012-02-09_")));
+//			scan.setFilter(filter1);
+//			scan.
+			
 			table=HTablePoolFactory.getPool().getTable("roi");
 			scanner=table.getScanner(scan);
 			
 			for (Result result : scanner) {
-				System.out.println(new String(result.getValue(Bytes.toBytes("cf"), Bytes.toBytes("tenantId"))));
+				//System.out.println(new String(result.getValue(Bytes.toBytes("cf"), Bytes.toBytes("tenantId"))));
+				System.out.println(result);
 			}
 			
 			return null;
